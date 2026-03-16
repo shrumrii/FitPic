6ed15eeb3f0c0c354b2c870fbeccc7da48a0f606
Hanoom Lee <hanoomlee@Hanooms-MacBook-Pro-2.local>
2026-03-15 19:48:31 -0500

    created navbar

--- frontend/frontend/app/dashboard/page.tsx
+++ frontend/frontend/app/dashboard/page.tsx
@@ -1,25 +1,78 @@
 "use client"; 
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import supabase from "@/lib/supabase";
 
 export default function Dashboard() {
     
     const router = useRouter(); 
+    const [username, setUsername] = useState(""); 
+    const [loading, setLoading] = useState(false); 
+
+    useEffect(() => { 
+        const getUsername = async () => { 
+            
+            setLoading(true); 
+
+            try { 
+                
+                const { data: { user }, error } = await supabase.auth.getUser(); 
+                console.log("data", user); 
+                
+                if (error) { 
+                    console.log("Supabase auth error, redirecting to welcome page"); 
+                    router.push("/welcome"); 
+                    return; 
+                }
+
+                if (!user) { 
+                    console.log("User not found, redirecting to welcome page"); 
+                    router.push("/welcome"); 
+                    return; 
+                }
+                
+                //check if uid in database 
+                const response = await fetch(`http://localhost:8000/users/${user.id}`); 
+                
+                if (!response.ok) { 
+                    console.log(await response.text())  
+                    throw new Error("Failed to get user id"); 
+                }
+
+                const result = await response.json();
+
+                if (!result.success) { 
+                    console.log(result.message); 
+                    console.log(`${user.id} has made an account, but has not created a profile. Redirecting to onboarding.`); 
+                    router.push("/onboarding"); 
+                    return; 
+                }
+
+                setUsername(result.data.username); 
+
+            } catch (error) { 
+                console.error("error", error); 
+                router.push("/welcome")
+            } finally { 
+                setLoading(false); 
+            }
+        }
+        getUsername(); 
+    }, [])
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                    Welcome page. 
-                </h1>
+        <div className="flex flex-col min-h-screen bg-zinc-100 font-sans dark:bg-black">
 
-                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                    <button onClick={() => router.push("/signup")}> Sign Up </button>
-                    <button onClick={() => router.push("/login")}> Log In </button>
-                </div>
+            <nav className="w-full bg-white dark:bg-zinc-900 px-8 py-4 flex items-center justify-between"> 
+                <h1 className="text-amber-400 font-semibold text-xl"> FitPic </h1>
+                <span className="text-amber-400 font-semibold text-xl"> {loading ? "..." : username} </span>
+            </nav>
+
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
                 
-                </div>
+                
             </main>
         </div>
     ); 
